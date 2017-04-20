df4e0d14781c2b17e4c9ff57affe990e57307dae
THUAN THANH LU <luthu@10-150-44-183.dhcp-m.deakin.edu.au>
2017-04-20 13:57:00 +1000

    SIT 206

---
+++ week6/week6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week6
+//
+//  Created by THUAN THANH LU on 20/04/2017.
+//  Copyright © 2017 THUAN THANH LU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
